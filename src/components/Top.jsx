8837d929e0bfea37e3337026739c14a2226eaa68
Shoaib Abdul Khadeer <shoaibabdulkhadeer@gmail.com>
2023-06-06 18:51:55 +0530

    cart icon

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -25,14 +25,14 @@ const Top = () => {
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="me-auto mx-3 my-3 my-lg-0">
+            className="me-auto mx-3 my-3 my-lg-0" style={{fontSize:"22px"}}>
 
             <Nav.Link >
-                <Link to='/'>Home </Link>
+                <Link to='/' style={{textDecoration:"none"}}>Home </Link>
              </Nav.Link>
 
             <Nav.Link > 
-                <Link to='/cart'>Cart:{cart.length}</Link>
+                <Link to='/cart' style={{textDecoration:"none"}} >🛒<b style={{color:"black"}}>{cart.length} </b></Link>
             </Nav.Link>
             <NavDropdown title="Link" id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
